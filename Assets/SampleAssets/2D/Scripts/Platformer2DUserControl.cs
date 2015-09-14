4d346082a7dcc81c6a358d2f676a575bb0287142
roschly <roschly@gmail.com>
2015-09-14 22:39:15 +0200

    Can see pixels and jump over boxes

--- Assets/SampleAssets/2D/Scripts/Platformer2DUserControl.cs
+++ Assets/SampleAssets/2D/Scripts/Platformer2DUserControl.cs
@@ -1,20 +1,23 @@
 ﻿using UnityEngine;
+using System.Collections;
 using UnitySampleAssets.CrossPlatformInput;
 
 namespace UnitySampleAssets._2D
 {
 
     [RequireComponent(typeof (PlatformerCharacter2D))]
+	//[RequireComponent(typeof (BehaviourScript))]
     public class Platformer2DUserControl : MonoBehaviour
     {
         private PlatformerCharacter2D character;
-        private bool jump;
+		private bool jump;
 
-		public Texture2D sourceTex;
+		public Texture2D tex;
 
         private void Awake()
         {
             character = GetComponent<PlatformerCharacter2D>();
+			tex = new Texture2D(640, 480, TextureFormat.RGB24, false);
         }
 
         private void Update()
@@ -23,7 +26,59 @@ namespace UnitySampleAssets._2D
 				// Read the jump input in Update so button presses aren't missed.
 				jump = CrossPlatformInputManager.GetButtonDown ("Jump");
 			}
+
+			StartCoroutine (captureScreen());
+			/*
+			// Read screen contents into the texture
+			tex.ReadPixels(new Rect(0, 0, 640, 480), 0, 0);
+
+			tex.Apply ();
+			
+			//byte[] bytes = tex.EncodeToPNG();
+			//System.IO.File.WriteAllBytes(Application.dataPath + "/../SavedScreen1.png", bytes);
+			
+			Color[] bv = tex.GetPixels();
+
+
+			Color test = new Color (0.247F, 0.710F, 0.851F, 1.000F);
+			if (test.ToString () == bv.GetValue (86600).ToString ()) {
+				character.Move(1,false,true);
+				//Debug.Log ("HEY HEY HEY HEY HEY HEY HEY HEY!!!!!");
+			}
+
+			//Debug.Log (test.ToString() + "  Bnana");
+			//Debug.Log (bv.GetValue (86500).ToString());
+			*/
+
         }
+
+
+		private IEnumerator captureScreen(){
+			yield return new WaitForEndOfFrame();
+
+			// Read screen contents into the texture
+			tex.ReadPixels(new Rect(0, 0, 640, 480), 0, 0);
+			
+			tex.Apply ();
+			
+			byte[] bytes = tex.EncodeToPNG();
+			System.IO.File.WriteAllBytes(Application.dataPath + "/../SavedScreen1.png", bytes);
+			
+			Color[] bv = tex.GetPixels();
+			
+			
+			Color test = new Color (0.247F, 0.710F, 0.851F, 1.000F);
+			if (test.ToString () == bv.GetValue (86600).ToString ()) {
+				character.Move(1,false,true);
+				//Debug.Log ("HEY HEY HEY HEY HEY HEY HEY HEY!!!!!");
+			}
+
+
+
+			//Debug.Log (test.ToString() + "  Bnana");
+			//Debug.Log (bv.GetValue (86500).ToString());
+		}
+
 
         private void FixedUpdate()
         {
@@ -31,6 +86,8 @@ namespace UnitySampleAssets._2D
             //bool crouch = Input.GetKey(KeyCode.LeftControl);
 			//float h = CrossPlatformInputManager.GetAxis("Horizontal");
             // Pass all parameters to the character control script.
+
+
 
 			character.Move(1, false, jump);
 			jump = false;
---
+++ Assets/Scripts/BehaviourScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnitySampleAssets._2D
+{
+	[RequireComponent(typeof (PlatformerCharacter2D))]
+	public class BehaviourScript : MonoBehaviour {
+
+		private PlatformerCharacter2D character;
+		public Transform player;
+		private int count = 0;
+
+		private void Awake()
+		{
+			character = GetComponent<PlatformerCharacter2D>();
+			
+		}
+
+		private void Update()
+		{	
+			Texture2D tex = new Texture2D(50, 25, TextureFormat.RGB24, false);
+			
+			// Read screen contents into the texture
+			tex.ReadPixels(new Rect(90, 123, 50, 25), 0, 0);
+			tex.Apply ();
+			
+			byte[] bytes = tex.EncodeToPNG();
+			System.IO.File.WriteAllBytes(Application.dataPath + "/../SavedScreen.png", bytes);
+			
+			Color[] bv = tex.GetPixels();
+			
+			Color test = new Color (0.129F, 0.694F, 0.302F, 1F);
+			if (test.ToString () == bv.GetValue (0).ToString ()) {
+				character.Move(1,false,true);
+			}
+			//Debug.Log (test.ToString() + "  Bnana");
+			//Debug.Log (bv.GetValue (0).ToString());
+
+			
+		}
+
+		public bool neuralTest(int bla){
+			if (bla != 0) {
+				return true;
+			} else {
+				return false;
+			}
+				
+		}
+
+	}
+}
